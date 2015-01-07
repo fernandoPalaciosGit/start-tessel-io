20c3b0b51d0fd180586ace77b5595eb72f543cbc
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-01-07 23:46:25 +0100

    set precision, scale and timer coords changes

--- accelerometer/streamCoords.js
+++ accelerometer/streamCoords.js
@@ -1,18 +1,31 @@
 var	tessel = require('tessel'),
-		accel = require('accel-mma84').use( tessel.port['A'] );
+		accel = require('accel-mma84').use( tessel.port['B'] );
 
-accel.on('ready', function( evReady ){
-
+var showDataAccelerometer = function( timer ){
+	// show data from interval
 	setInterval(function(){
 
+		// reload data listeners
+		accel.removeAllListeners('data');
+
 		accel.on('data', function( coords ){
+			
 			var	x = coords[0].toFixed(2),
 					y = coords[1].toFixed(2),
 					z = coords[2].toFixed(2);
 			console.info('X: ', x, 'Y: ', y, 'Z: ', z);
+		});
+	}, timer);
+}
 
-		}); // ready to show
-	}, 500); // show interval data
+accel.on('ready', function( evReady ){
+
+	// set precision and scale
+	accel.setOutputRate(1.56, function rateSet() {
+		accel.setScaleRange( 8, function scaleSet() {
+			showDataAccelerometer( 1000 );
+		});
+	});
 });
 
 accel.on('error', function( evErr ){
